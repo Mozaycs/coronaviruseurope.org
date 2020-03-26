7286026cc89002a4f0038f6936bd0ef6072c10b4
Darshan kumar <darshankumar38@gmail.com>
2020-03-26 22:09:49 +0100

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,26 @@ module.exports = {
       },
     ],
   },
-  plugins: [`gatsby-plugin-emotion`, `gatsby-plugin-react-leaflet`],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-leaflet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-162018035-1",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-162018035-1`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
+  ],
 }
