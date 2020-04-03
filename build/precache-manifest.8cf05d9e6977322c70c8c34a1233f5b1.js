57b23eacdaab2d4f9c187fc339d2deb265e9e8f1
Darshan kumar <darshankumar38@gmail.com>
2020-04-04 01:16:47 +0200

    build changes for map version

---
+++ build/precache-manifest.8cf05d9e6977322c70c8c34a1233f5b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c872e3e3785aa3fcaf3069300a51a34a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d88cc0ca0fe1de927c",
+    "url": "/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "75c4606f4321d9b3dab3",
+    "url": "/static/css/main.a291d4f6.chunk.css"
+  },
+  {
+    "revision": "19d88cc0ca0fe1de927c",
+    "url": "/static/js/2.98366e81.chunk.js"
+  },
+  {
+    "revision": "3b9121933965c5cc15b802525ae1ee7f",
+    "url": "/static/js/2.98366e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c4606f4321d9b3dab3",
+    "url": "/static/js/main.70b1a611.chunk.js"
+  },
+  {
+    "revision": "f23dfa1436fb8c35bc11",
+    "url": "/static/js/runtime-main.1296b635.js"
+  },
+  {
+    "revision": "386fab57c434b1fe24443591bed34f47",
+    "url": "/static/media/canada-provinces.386fab57.geojson"
+  },
+  {
+    "revision": "cf89f442467f1500fa6e50f924232d1e",
+    "url": "/static/media/china-provinces.cf89f442.geojson"
+  },
+  {
+    "revision": "3fa0f7b5e0340c333e7029c5ec56319d",
+    "url": "/static/media/us-states.3fa0f7b5.geojson"
+  }
+]);
